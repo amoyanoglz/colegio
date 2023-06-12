29e0944e0c1f9869520a4097416b0c947d364e37
LAPTOP-185UEBDF\amoya <Andres>
2023-06-12 22:24:12 +0200

    ejemplo request mia

---
+++ request_mia.py
@@ -0,0 +1,10 @@
+import json
+import requests
+
+urlData = 'https://api.coinbase.com/v2/exchange-rates?currency=EUR'
+# urlData = 'https://randomuser.me/api/?results=' + input("Cuantos quieres? ")
+
+webURL = requests.get(urlData)
+
+posts = webURL.json()
+print(posts)
